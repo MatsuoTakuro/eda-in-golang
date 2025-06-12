b033ad2c48160b8f515834a5d3160e2508f1bf21
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2025-06-12 19:18:06 +0900

    feat: 🎸 add outbox and inbox tables for each module

--- docker/database/008_create_search_schema.sh
+++ docker/database/008_create_search_schema.sh
@@ -56,6 +56,30 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "mallbots" <<-EOSQL
     PRIMARY KEY (order_id)
   );
 
+  CREATE TRIGGER updated_at_sorders_trgr BEFORE UPDATE ON search.orders FOR EACH ROW EXECUTE PROCEDURE updated_at_trigger();
+
+  CREATE TABLE search.inbox
+  (
+    id          text NOT NULL,
+    name        text NOT NULL,
+    subject     text NOT NULL,
+    data        bytea NOT NULL,
+    received_at timestamptz NOT NULL,
+    PRIMARY KEY (id)
+  );
+
+  CREATE TABLE search.outbox
+  (
+    id           text NOT NULL,
+    name         text NOT NULL,
+    subject      text NOT NULL,
+    data         bytea NOT NULL,
+    published_at timestamptz,
+    PRIMARY KEY (id)
+  );
+
+  CREATE INDEX search_unpublished_idx ON search.outbox (published_at) WHERE published_at IS NULL;
+
   GRANT USAGE ON SCHEMA search TO mallbots_user;
   GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA search TO mallbots_user;
 EOSQL
